920b3e2e076e286c8f6e6324e6baddcd2734455d
ronald.delangen@mybrand.nl <ronald.delangen@mybrand.nl>
2023-04-25 16:53:45 +0200

    Start test setup

--- one/AdventOne.java
+++ one/AdventOne.java
@@ -1,7 +1,5 @@
-package adventOne;
+package adventOfCode.AdventOfCode.one;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -9,34 +7,10 @@ public class AdventOne {
 
 	public static void main(String[] args) throws IOException{
 		
-		ArrayList<Integer> integerList = new ArrayList<>();
+		String filename = "C:\\Users\\ronald.delangen\\eclipse-workspace\\JAVA_Tutorial\\src\\adventOfCode\\AdventOfCode\\one\\Test.txt";
 		
-		BufferedReader reader = null;
-		try {
-			reader = new BufferedReader(new FileReader("C:\\Users\\ronald.delangen\\eclipse-workspace\\JAVA_Tutorial\\src\\adventOne\\Test.txt"));
-			
-			Integer n = 0;
-			
-			String c = reader.readLine();
-			while (c != null) {
-				System.out.println(c);
-				
-				if (c.equals("")) {
-					integerList.add(n);
-					n = 0;
-				}else {
-					n = n + Integer.parseInt(c);
-				}
-				c = reader.readLine();
-			}
-			integerList.add(n);
-		} finally {
-			if (reader != null){
-				reader.close();
-			}
-		}
-		
-		System.out.println("\n");
+		AdventOneCore core = new AdventOneCore();
+		ArrayList<Integer> integerList = core.getIntegerList(filename);
 		
 		for(Integer i:integerList) {
 			System.out.println(i);
---
+++ one/AdventOneCoreTest.java
@@ -0,0 +1,14 @@
+package adventOfCode.AdventOfCode.one;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AdventOneCoreTest {
+
+	@Test
+	void testGetIntegerList() {
+		fail("Not yet implemented");
+	}
+
+}
